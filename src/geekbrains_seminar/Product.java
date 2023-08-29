81ebc1fab8257dc00c2c1d35e2308b869692e4ad
Andrey_Bryzgalov <bav03630363@gmail.com>
2023-08-29 12:18:14 +0300

    test29

--- src/geekbrains_seminar/Product.java
+++ src/geekbrains_seminar/Product.java
@@ -5,6 +5,29 @@ public class Product {
     String brand;
     double price;
 
+    public Product(){
+        this("Noname");
+    }
+
+    public Product(String name){
+        this(name, "Noname");
+    }
+
+    public Product(String name, String brand){
+        this(name, brand, 100);
+    }
+
+    public Product(String name, String brand, double price){
+        this.name = name;
+        this.brand = brand;
+        this.price = price;
+
+    }
+
+    public String displayInfo(){
+        return String.format("%s - %s - %.2f", name, brand, price);
+    }
+
 
 
 }
--- src/geekbrains_seminar/Program_vending.java
+++ src/geekbrains_seminar/Program_vending.java
@@ -3,6 +3,16 @@ package geekbrains_seminar;
 public class Program_vending {
     public static void main(String[] args) {
         Product product_1 = new Product();
+        product_1.name = "Product_1";
+        product_1.brand = "Brand_1";
+        product_1.price = 200;
+        System.out.println(product_1.displayInfo());
+
+        Product product_2 = new Product("Product_2", "Brand_2", 300);
+        System.out.println(product_2.displayInfo());
+
+        Product product_3 = new Product();
+        System.out.println(product_3.displayInfo());
 
 
     }
